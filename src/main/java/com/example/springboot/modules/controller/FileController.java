12e7973a0aab7edba4d75ac90eaa90fba541ac23
dev-rui <18655377896@163.com>
2023-03-27 15:27:40 +0800

    增加文件在线预览功能（将word、excel、ppt等转换成pdf）

---
+++ src/main/java/com/example/springboot/modules/controller/FileController.java
@@ -0,0 +1,29 @@
+package com.example.springboot.modules.controller;
+
+import com.example.springboot.modules.service.FileService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 文件服务控制类
+ *
+ * @author heruihong
+ * @createTime 2023-03-27 15:19:46
+ */
+
+public class FileController {
+
+
+    @Autowired
+    private FileService fileService;
+
+    @ApiOperation(value = "系统文件在线预览接口")
+    @PostMapping("/api/file/onlinePreview")
+    public void onlinePreview(@RequestParam("url") String url, HttpServletResponse response) throws Exception{
+        fileService.onlinePreview(url,response);
+    }
+}
---
+++ src/main/java/com/example/springboot/modules/service/FileService.java
@@ -0,0 +1,13 @@
+package com.example.springboot.modules.service;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 文件服务
+ *
+ * @author heruihong
+ * @createTime 2023-03-27 15:17:06
+ */
+public interface FileService {
+    void onlinePreview(String url, HttpServletResponse response) throws Exception;
+}
